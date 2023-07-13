2772ebcea3c2d42fe2a6a285d14f98c2cf0e7e41
vickouma77 <Vickprestone20@gmail.com>
2023-07-13 14:46:19 +0300

    creating table user

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
--- script that creates a table users
--- has id, email, name and country
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country VARCHAR(255) ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
-)
-
+-- Script that creates a table users following:
+-- id, email, name, country
+CREATE TABLE IF NOT EXISTS users(
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
+);
