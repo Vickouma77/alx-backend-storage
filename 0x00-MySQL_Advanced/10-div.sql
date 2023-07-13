85d4029506b48e6540cc7ca62eae7bc45cc1e440
vickouma77 <Vickprestone20@gmail.com>
2023-07-13 22:44:31 +0300

    10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- script that creates a view need_meeting that lists all 
+-- students that have a score under 80 (strict) and no last_meeting
+
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
+DELIMITER ;
