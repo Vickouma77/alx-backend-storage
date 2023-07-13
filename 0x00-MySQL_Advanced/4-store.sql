a6fc23448fc0433fbcdb4b7d65977f62fd1748f6
vickouma77 <Vickprestone20@gmail.com>
2023-07-13 18:02:35 +0300

    4-store

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -2,4 +2,4 @@
 -- of an item after adding a new order.
 
 CREATE TRIGGER update_quantity AFTER INSERT ON orders FOR EACH ROW
-UPDATE items SET quantity = quantity - 1 WHERE items.id = NEW.item_id;
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
