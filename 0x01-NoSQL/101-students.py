b4fd590530569344d98e0d6b730b4dedf775c8f2
vickouma77 <Vickprestone20@gmail.com>
2023-07-18 19:54:43 +0300

    101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Top students
+"""
+
+
+from pymongo import mongo_client
+
+
+def top_students(mongo_collection):
+    """
+    function that returns all students sorted by average score
+    """
+    return mongo_collection.aggregate([
+        {"$project": {"name": "$name", "averageScore": {
+            "$avg": "$topics.score"}}},
+        {"$sort": {"averageScore": -1}}
+    ])
