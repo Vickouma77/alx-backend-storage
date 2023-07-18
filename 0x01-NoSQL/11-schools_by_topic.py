f3ae5d9b93471de80f2f0a96024cb3d2a15ab650
vickouma77 <Vickprestone20@gmail.com>
2023-07-18 19:07:45 +0300

    11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,12 +4,11 @@ Where can I learn Python?
 """
 
 
-from pymongo import MongoClient
+import pymongo
 
 
 def schools_by_topic(mongo_collection, topic):
     """
     returns the list of school having a specific topic
     """
-    
     return mongo_collection.find({"topic": "topic"})
