b44bff81d37a8478832fee7e9a2e79f8b58637bf
vickouma77 <Vickprestone20@gmail.com>
2023-07-18 20:12:28 +0300

    102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -7,16 +7,35 @@ Log stats - new version
 from pymongo import MongoClient
 
 
-def log_stats(mongo_collection, option=None):
+def nginx_stats():
     """
     provides some stats about Nginx logs stored in MongoDB
     """
-    if option is None or not mongo_collection:
-        return 0
-    if option == "method":
-        return mongo_collection.count_documents({"method": "GET"})
-    if option == "path":
-        return mongo_collection.count_documents(
-            {"method": "GET", "path": "/status"}
-        )
-    return mongo_collection.count_documents({})
+    client = MongoClient('mongodb://localhost:27017')
+    logs = client.logs.nginx
+
+    num_docs = logs.count_documents({})
+    print(f"{num_docs} logs")
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        num_method = logs.count_documents({"method": method})
+        print(f"\tmethod {method}: {num_method}")
+    check = logs.count_documents({"method": "GET", "path": "/status"})
+    print(f"{check} status check")
+
+    print("IPs:")
+    top_ips = logs.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    for ip in top_ips:
+        count = ip.get("count")
+        ip_id = ip.get("_id")
+        print(f"\t{ip_id}: {count}")
+
+
+if __name__ == "__main__":
+    nginx_stats()
