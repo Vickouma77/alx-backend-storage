ddad63a7e7379db50743ce3f61c7a3fadcd4e561
vickouma77 <Vickprestone20@gmail.com>
2023-07-18 17:37:37 +0300

    9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+insert a document in Python
+"""
+
+
+from pymongo import mongo_client
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    insert a document in python
+    """
+    if not mongo_collection:
+        return None
+    return mongo_collection.insert_one(kwargs).new_id
